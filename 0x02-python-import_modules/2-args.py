45f1c394d5d97256addcf63a29c5350e7d8fde01
Jumoc <juanpcaicedo2@gmail.com>
2020-12-10 17:10:32 -0500

    Fix output check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) == 2:
         print("{:d} argument:".format(len(argv) - 1))
+    elif len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
     else:
         print("{:d} arguments:".format(len(argv) - 1))
     i = 1
