2ad27ede3251ff9e4c7045b57f860e8172154b2d
Jumoc <juanpcaicedo2@gmail.com>
2020-12-10 18:21:43 -0500

    Fix missing pep8 check

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-   dirs = dir(hidden_4)
-   dirs.sort()
-   for d in dirs:
-      if(d[0] != "_"):
-          print(d)
+    dirs = dir(hidden_4)
+    dirs.sort()
+    for d in dirs:
+        if(d[0] != "_"):
+            print(d)
