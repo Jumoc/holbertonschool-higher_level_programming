11d3633f8c08bd968a47ca14b928119c873b2ef1
Jumoc <juanpcaicedo2@gmail.com>
2020-12-10 21:00:42 -0500

    Fix code try

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 from sys import argv, __stderr__
 if __name__ == "__main__":
     if len(argv) != 4:
@@ -12,15 +12,15 @@ if __name__ == "__main__":
     result = 0
     index = 0
     if argv[2] == operators[0]:
-        result = calc.add(int(argv[1]), int(argv[3]))
+        result = add(int(argv[1]), int(argv[3]))
         index = 0
     elif argv[2] == operators[1]:
-        result = calc.sub(int(argv[1]), int(argv[3]))
+        result = sub(int(argv[1]), int(argv[3]))
         index = 1
     elif argv[2] == operators[2]:
-        result = calc.mul(int(argv[1]), int(argv[3]))
+        result = mul(int(argv[1]), int(argv[3]))
         index = 2
     elif argv[2] == operators[3]:
-        result = calc.div(int(argv[1]), int(argv[3]))
+        result = div(int(argv[1]), int(argv[3]))
         index = 3
     print("{} {} {} = {}".format(argv[1], operators[index], argv[3], result))
