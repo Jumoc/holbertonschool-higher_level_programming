f3d0d7efa608b5d6ea5b260be7f6939824cc0ccf
Jumoc <juanpcaicedo2@gmail.com>
2020-12-10 20:41:17 -0500

    Code try

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ import calculator_1 as calc
 from sys import argv, __stderr__
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
     operators = "+-*/"
     if operators.find(argv[2]) == -1:
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     elif argv[2] == operators[3]:
         result = calc.div(int(argv[1]), int(argv[3]))
         index = 3
-    print("{} {} {} = {}".format(argv[1], operators[index], argv[3], result))
+    print("{} {} {} = {}".format(argv[0], operators[index], argv[3], result))
