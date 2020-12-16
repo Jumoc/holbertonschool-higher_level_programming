683ce8e76eeaba07442e01a1088825d5aef6dfda
Jumoc <juanpcaicedo2@gmail.com>
2020-12-15 23:39:30 -0500

    Fix output error

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * list_len - returns the size of a list
+ *
+ * @h: header of the singly linked list
+ *
+ * Return: number of nodes
+ */
+
+int list_len(listint_t *h)
+{
+	int i = 0;
+	listint_t *current = h;
+
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
+
+/**
  * is_palindrome - checks if a s linked list is a palindrome
  * @head: head of the list
  * Return: 1 if palindrome 0 otherwise
@@ -12,20 +33,16 @@ int is_palindrome(listint_t **head)
 
 	if (!*head)
 		return (0);
+	size = list_len(*head);
+	if (size == 1)
+		return (1);
+	size--;
 	current = *head;
 	tail = *head;
-	while (tail->next)
-	{
-		tail = tail->next;
-		size++;
-	}
 	for (i = 0; i < (size / 2) + 1; i++)
 	{
 		tail = *head;
-		for (j = 0; j < (size - i); j++)
-		{
-			tail = tail->next;
-		}
+		while
 		if (current->n != tail->n)
 			return (0);
 		current = current->next;
