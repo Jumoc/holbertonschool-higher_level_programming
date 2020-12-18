ee924de8a3a45c5be5c7ba0aa40570b7b2910eba
Jumoc <juanpcaicedo2@gmail.com>
2020-12-18 13:59:33 -0500

    Fix pep8 check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,18 +4,14 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str:
         return result
     numbers = {
-        "I":1,
-        "V":5,
-        "X":10,
-        "L":50,
-        "C":100,
-        "D":500,
-        "M":1000
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
         }
     for index in range(0, len(roman_string)):
         number = numbers.get(roman_string[index])
-        if index != (len(roman_string) - 1) and numbers.get(roman_string[index + 1]) > number:
-            result -= number
+        if index != (len(roman_string) - 1):
+            if numbers.get(roman_string[index + 1]) > number:
+                result -= number
         else:
             result += number
     return result
