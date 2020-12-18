89d011a9ef3967c9ea243ca10e261af6e7738ef4
Jumoc <juanpcaicedo2@gmail.com>
2020-12-17 22:25:55 -0500

    Fix output check

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = []
-    [new_set.append(element) for element in set_1]
-    [new_set.append(element) for element in set_2]
+    for element in set_1:
+        if not get_uniq(set_2, element):
+            new_set.append(element)
+    for element in set_2:
+        if not get_uniq(set_1, element):
+            new_set.append(element)
     return set(new_set)
+
+
+def get_uniq(mlist, n):
+    for element in mlist:
+        if element is n:
+            return True
+    return False
