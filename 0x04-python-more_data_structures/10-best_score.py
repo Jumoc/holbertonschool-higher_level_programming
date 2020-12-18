e7900b34383dabfa8fa3bc9c388ca961e117b4c4
Jumoc <juanpcaicedo2@gmail.com>
2020-12-17 23:43:25 -0500

    Fix output check

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
             return None
-    greaterValue = -1000
-    greaterKey = ""
+    greaterValue = -100000
+    greaterKey = None
     for key in a_dictionary.keys():
         if a_dictionary.get(key) > greaterValue:
             greaterValue = a_dictionary.get(key)
