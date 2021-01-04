1dfc5760b27331cf0e88c5ba885c3aeaa8d649c6
Jumoc <juanpcaicedo2@gmail.com>
2021-01-04 15:12:50 -0500

    Add py function that prints an integer safely

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as ve:
+        print("Error: {}".format(ve), file=stderr)
+        return False
+    else:
+        return True
