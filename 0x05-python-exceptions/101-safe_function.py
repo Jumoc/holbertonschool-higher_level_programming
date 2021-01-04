857975e6c6cbfe2952d34a8c1ea475210e55e065
Jumoc <juanpcaicedo2@gmail.com>
2021-01-04 15:24:27 -0500

    Add py function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    value = None
+    try:
+        value = fct(args[0], args[1])
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=stderr)
+    finally:
+        return value
