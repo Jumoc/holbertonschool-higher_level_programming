277edf672347669dedd074c57e796e655bc2d75d
Jumoc <juanpcaicedo2@gmail.com>
2021-01-04 15:27:26 -0500

    Fix missing output check

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,11 +3,8 @@ from sys import stderr
 
 
 def safe_function(fct, *args):
-    value = None
     try:
-        value = fct(args[0], args[1])
+         return fct(args[0], args[1])
     except Exception as ex:
         print("Exception: {}".format(ex), file=stderr)
-        return value
-    else:
-        return value
+        return None
