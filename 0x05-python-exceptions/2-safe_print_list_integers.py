89c14e18b90d2b200a7cb8f567831522a137f133
Jumoc <juanpcaicedo2@gmail.com>
2021-01-04 13:35:37 -0500

    Fix missing output check

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i, j = -1, 0
+    i, j = 0, 0
     while i < x:
-        i += 1
         try:
             print("{:d}".format(my_list[i]), end="")
         except (TypeError, ValueError):
+            i += 1
             continue
-        except IndexError:
-            break
         else:
             j += 1
+            i += 1
     print("")
     return j
