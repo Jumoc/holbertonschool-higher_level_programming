a128db884063f8f9c8510dba76a30a5eed845bbc
Jumoc <juanpcaicedo2@gmail.com>
2021-01-04 15:14:29 -0500

    Fix missing output checks

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from sys import stderr
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as ve:
         print("Error: {}".format(ve), file=stderr)
+        return False
+    except TypeError as te:
+        print("Error: {}".format(te), file=stderr)
         return False
     else:
         return True
