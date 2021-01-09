ab709b50e7d3b44c60f8a3a634c3704563c18c3e
Jumoc <juanpcaicedo2@gmail.com>
2021-01-08 21:59:59 -0500

    Add py function that adds two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is the module description"""
+
+
+def add_integer(a, b=98):
+    """Function to add to integers
+    Args:
+        a: first number
+        b: second number"""
+    types = (int, float)
+    if type(a) not in types:
+        raise TypeError("a must be an integer")
+    if type(b) not in types:
+        raise TypeError("b must be an integer")
+    return int(a + b)
