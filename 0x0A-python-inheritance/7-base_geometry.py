58b56bba86478d738f612dd346dc570ba0cac8de
Jumoc <juanpcaicedo2@gmail.com>
2021-01-20 00:01:57 -0500

    Fix code try

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""This is a module definition"""
+"""This is a module definition for Base geometry
+    Class:
+        BaseGeometry"""
 
 
 class BaseGeometry():
-    """BaseGeometry class creates base geometry"""
+    """BaseGeometry class creates base geometry
+        for any geometric shape"""
     def area(self):
-        """Raises an exception on call"""
+        """Raises an exception on call to get implemented
+            on subclasses"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is a positive integer"""
+        """Validates that value is a positive integer
+            Args:
+                name: name of the value
+                value: value to be checked"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
