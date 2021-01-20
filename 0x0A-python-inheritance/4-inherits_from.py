ea65d4ae34ee5b84745a72a28c8c6a0edd910af4
Jumoc <juanpcaicedo2@gmail.com>
2021-01-19 23:56:30 -0500

    Fix missing check

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,7 @@
 
 def inherits_from(obj, a_class):
     """MyList class that inherits from class list"""
-    return isinstance(type(obj), a_class)
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
