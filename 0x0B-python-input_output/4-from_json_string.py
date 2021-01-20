a636a78b687c805e8453321a82aa8d02464ce538
Jumoc <juanpcaicedo2@gmail.com>
2021-01-20 18:23:39 -0500

    Add finction that converts json to python

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module description for to_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """returns the json representation of an object"""
+    return json.loads(my_str)
