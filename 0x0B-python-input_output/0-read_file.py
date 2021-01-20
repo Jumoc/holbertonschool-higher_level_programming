081b9746ef5e7ab590b4b3bc459284642be0ab36
Jumoc <juanpcaicedo2@gmail.com>
2021-01-20 18:03:14 -0500

    Fix checks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """prints the contents of a given file"""
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, encoding="utf-8") as file:
         print(file.read())
