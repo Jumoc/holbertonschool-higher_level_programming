074866765126b963a5133caea53b7ae3852624e2
Jumoc <juanpcaicedo2@gmail.com>
2021-01-19 23:52:56 -0500

    Fix check

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,8 +6,8 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """BaseGeometry class creates base geometry"""
     def __init__(self, size):
+        super().integer_validator("size", size)
         super().__init__(size, size)
-        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
