2f8f7f336ca52b8d40ee5b86d33d8eb91dde8222
Jumoc <juanpcaicedo2@gmail.com>
2021-01-20 19:24:41 -0500

    Fix missing check

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,4 +5,12 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-save_to_json_file(sys.argv[1:], "add_item.json")
+file = "add_item.json"
+try:
+    open(file, "r", encoding="utf-8")
+except:
+    save_to_json_file(sys.argv[1:], file)
+else:
+    m_list = load_from_json_file(file)
+    m_list += (sys.argv[1:])
+    save_to_json_file(m_list, file)
