1bc11c5730c3a51af95e058e964b4b09653b09c9
Jumoc <juanpcaicedo2@gmail.com>
2021-01-20 21:25:51 -0500

    Updated to_json method

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module description for Student"""
+
+
+class Student():
+    """Defines a student with names and age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns the dict of the object"""
+        if type(attrs) != list:
+            return self.__dict__
+        else:
+            n_dict = {}
+            for att in attrs:
+                value = self.__dict__.get(att)
+                if value is not None:
+                    n_dict[att] = value
+        return n_dict
