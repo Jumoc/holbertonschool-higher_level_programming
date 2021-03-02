e32b884851a5ea958e329cad4e5c64bfce16ee1f
Jumoc <juanpcaicedo2@gmail.com>
2021-03-02 01:10:50 -0500

    Fix avg temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- print temperatures
-SELECT city, AVG(value) AS avg_temp FROM temperatures ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
