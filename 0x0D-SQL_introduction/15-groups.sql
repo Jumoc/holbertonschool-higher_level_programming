5ae6b8e5fe5b0f7e312abf372859f0381b6a4b66
Jumoc <juanpcaicedo2@gmail.com>
2021-03-02 00:12:21 -0500

    Fix missing the ordering

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Number of records with the same score
-SELECT score, COUNT(id) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(id) AS number FROM second_table GROUP BY score ORDER BY number DESC;
