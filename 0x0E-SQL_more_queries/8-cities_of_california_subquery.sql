3b1455a968c4778cf4d705dbb28c8595e9b3e046
Jumoc <juanpcaicedo2@gmail.com>
2021-03-02 21:43:08 -0500

    Add script to search a city

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- cities of california
-SELECT id, names FROM cities WHERE (SELECT id FROM states WHERE name = 'California');
+SELECT id, name FROM cities WHERE (SELECT id FROM states WHERE name = 'California');
