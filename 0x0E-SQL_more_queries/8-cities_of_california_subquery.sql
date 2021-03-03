7bd783dc52c942acadb0ad97f616c3cfccf14cac
Jumoc <juanpcaicedo2@gmail.com>
2021-03-02 22:02:55 -0500

    Fix syntax error

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- cities of california
-SELECT id, name FROM cities WHERE (SELECT id FROM states WHERE name = 'California');
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
