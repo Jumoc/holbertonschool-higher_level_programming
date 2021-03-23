7a623c75faf7442bc1800610732d9731d943344e
Jumoc <juanpcaicedo2@gmail.com>
2021-03-23 18:19:14 -0500

    Fix missing shebang

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 exports.esrever = function (list) {
   for (let i = 0; i < Math.round(list.length / 2); i++) {
     [list[i], list[list.length - 1 - i]] = [list[list.length - 1 - i], list[i]];
