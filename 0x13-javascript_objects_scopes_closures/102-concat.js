9c47572ec48fac0215e3bda5010f74ef70bfd342
Jumoc <juanpcaicedo2@gmail.com>
2021-03-23 20:22:43 -0500

    Fix added new line at the end of both files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 try {
   const file1 = fs.readFileSync(process.argv[2], 'utf8');
   const file2 = fs.readFileSync(process.argv[3], 'utf8');
-  const data = file1 + '\n' + file2;
+  const data = file1 + '\n' + file2 + '\n';
   fs.writeFileSync(process.argv[4], data);
 } catch (err) {
   console.error(err);
