9abc8d69b430e9dbd6cbf445196ba6cbf19bf31b
Jumoc <juanpcaicedo2@gmail.com>
2021-03-27 17:23:14 +0000

    Fix pep8

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         """SELECT cities.name
-         FROM cities JOIN states WHERE cities.state_id=(SELECT id FROM states WHERE name=%(name)s)
+         FROM cities JOIN states
+         WHERE cities.state_id=(SELECT id FROM states WHERE name=%(name)s)
          GROUP BY cities.id
          ORDER BY cities.id ASC""",
-         {"name": sys.argv[4]}
+        {"name": sys.argv[4]}
         )
     query_rows = cur.fetchall()
     for i in range(len(query_rows)):
