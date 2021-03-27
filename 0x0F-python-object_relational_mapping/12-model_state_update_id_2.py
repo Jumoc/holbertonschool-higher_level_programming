9cbe7b9603932afea40895cdb85c0fc03407499f
Jumoc <juanpcaicedo2@gmail.com>
2021-03-27 17:36:43 +0000

    Fix pep8

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
+"""Module state update id"""
+
 from sqlalchemy import create_engine
 from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 import sys
 
 if __name__ == "__main__":
-    #Create engine that connects to the core (MySQL)
+    # Create engine that connects to the core (MySQL)
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.
         format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
         )
-    #Creates metadata and table schemes
+    # Creates metadata and table schemes
     Base.metadata.create_all(engine)
-    #Get the class of session and bind it with the engine
+    # Get the class of session and bind it with the engine
     Session = sessionmaker(bind=engine)
-    #Instantiate the session
+    # Instantiate the session
     session = Session()
-    #The session object manages all the transactions
+    # The session object manages all the transactions
     result = session.query(State).filter_by(id=2).one_or_none()
     if result is not None:
         result.name = "New Mexico"
