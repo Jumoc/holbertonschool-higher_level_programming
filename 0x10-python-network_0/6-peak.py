c26b5f8833635e07a1b5999040cd87c59cb2fa91
Jumoc <juanpcaicedo2@gmail.com>
2021-04-08 17:59:44 -0500

    Fix: syntax of sort wrong

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,6 @@
 
 def find_peak(list_of_integers):
     """Finds the peak in a list of integers"""
-
     if len(list_of_integers) != 0:
-        return sorted(list_of_integers)[-1]
+        list_of_integers.sort()
+        return list_of_integers[-1]
