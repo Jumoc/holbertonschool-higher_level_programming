2a1728fc44402a46ec9a38537f7b3f29ffd22308
Jumoc <juanpcaicedo2@gmail.com>
2021-04-08 17:27:25 -0500

    Changed to sort function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,11 +4,5 @@
 def find_peak(list_of_integers):
     """Finds the peak in a list of integers"""
 
-    if len(list_of_integers) == 0:
-        return None
-
-    avg = sum(list_of_integers) / len(list_of_integers)
-
-    for n in list_of_integers:
-        if (n >= avg):
-            return n
+    if len(list_of_integers) != 0:
+        return sorted(list_of_integers)[-1]
