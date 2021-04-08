89da42804409facaedd75b5435065f44460984c1
Jumoc <juanpcaicedo2@gmail.com>
2021-04-08 16:23:14 -0500

    Fix: check if no status check is needed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Displays the body of a response if status equals 200
-status=$(curl -LsI "$1" | tail -6 | head -1 | cut -d " " -f 2)
-if [ "$status" == "200" ]; then
-    curl -sL "$1"
-fi
+curl -sL "$1"
