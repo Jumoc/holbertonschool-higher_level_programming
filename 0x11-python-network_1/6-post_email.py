04d26a6e0668b691c4d4cd1c9695174f07fd6045
Jumoc <juanpcaicedo2@gmail.com>
2021-04-09 16:29:55 -0500

    Fix: changed verb

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""hbtn status module"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    request = requests.post(sys.argv[1], data = {'email': sys.argv[2]})
+    print(request.text)
