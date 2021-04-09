20b7911768fc1d9728a2c1cc0ac9609791783e6e
Jumoc <juanpcaicedo2@gmail.com>
2021-04-09 16:19:15 -0500

    Fix: change module for requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """hbtn status module"""
-from urllib import request
+import requests
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        content = response.read().decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}\n\t- content: {}".format(
-            type(content), content
-        ))
+    request = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}\n\t- content: {}".format(
+        type(request.text), request.text
+    ))
