03147e41036ac85cd05f31a2e15e4a0c998d9e4a
Jumoc <juanpcaicedo2@gmail.com>
2021-04-09 17:51:56 -0500

    Fix: added method .json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,10 +8,12 @@ if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
     request = requests.post(url, data={'q': letter})
-    r_dict = eval(request.text)
-    if type(r_dict) is not dict:
+    try:
+        r_dict = request.json()
+    except ValueError:
         print("Not a valid JSON")
-    elif r_dict.get('name') is None:
+
+    if r_dict.get('name') is None:
         print("No result")
-    elif "{" in request.text and "}" in request.text:
+    else:
         print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
