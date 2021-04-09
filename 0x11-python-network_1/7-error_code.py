9ee7cd05822ab46bc20a49ddf8d8afa53f608821
Jumoc <juanpcaicedo2@gmail.com>
2021-04-09 16:55:22 -0500

    Fix: typo line 10

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     request = requests.get(sys.argv[1])
     if int(request.status_code) >= 400:
-        print("Error code: {}".format(frequest.status_code))
+        print("Error code: {}".format(request.status_code))
     else:
         print(request.text)
