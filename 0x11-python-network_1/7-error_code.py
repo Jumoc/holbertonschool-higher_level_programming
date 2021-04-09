9819da3aeaf9aa0a958800a6fa25ab2e1a464674
Jumoc <juanpcaicedo2@gmail.com>
2021-04-09 16:48:09 -0500

    Add script that sends a request and prints errors if any

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""hbtn status module"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    request = requests.get(sys.argv[1])
+    if int(request.status_code) >= 400:
+        print("Error code: {}".format(frequest.status_code))
+    else:
+        print(request.text)
