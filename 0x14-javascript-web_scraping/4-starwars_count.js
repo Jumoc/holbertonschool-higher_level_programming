8d1264abcf937a5102daf490b9a225a162c361b6
Jumoc <juanpcaicedo2@gmail.com>
2021-05-04 23:54:34 +0000

    Update script's way of getting the data

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
+const process = require('process');
 
-request('https://swapi-api.hbtn.io/api/people/18/', (err, resp, body) => {
+const url = process.argv[2];
+
+request(url, (err, resp, body) => {
   if (err) console.error(err);
-  console.log(JSON.parse(body).films.length);
+  // console.log(JSON.parse(body));
+  let counter = 0;
+  for (const result of JSON.parse(body).results) {
+      result.characters.forEach(element => {
+          if (element.includes('18')) {
+              counter++;
+          }
+      });
+  }
+  console.log(counter);
 });
