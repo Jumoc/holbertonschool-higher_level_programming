d8661b7ee08bf0af07370a3ce1e5937d59935591
Jumoc <juanpcaicedo2@gmail.com>
2021-05-04 20:23:53 +0000

    Add script that counts the number of movies a character was in

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 
 request('https://swapi-api.hbtn.io/api/people/18/', (err, resp, body) => {
   if (err) console.error(err);
